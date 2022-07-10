0e24a406f21e9bf8fbf09943078b7faf8df3bd35
Robert Knight <robertknight@gmail.com>
2022-07-10 08:23:24 +0100

    Revise detection model to be more flexible
    Revise the U-Net-like model to be more flexible with respect to the
    number of levels and the number of channels from the output of each
    level.
    
    In the process the depth scale has been adjusted to more aggressively
    downsample the inputs with a small number of feature channels in the
    early layers. This will be combined with a change in training to use a
    higher-resolution input, in order to help more accurately segment small
    text lines that are closely spaced, without increasing inference time
    too much.

--- text_detection/model.py
+++ text_detection/model.py
@@ -3,7 +3,7 @@ from torch import nn
 
 
 class DepthwiseConv(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels: int, out_channels: int):
         super().__init__()
 
         self.seq = nn.Sequential(
@@ -20,12 +20,12 @@ class DepthwiseConv(nn.Module):
             nn.ReLU(),
         )
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor):
         return self.seq(x)
 
 
 class DoubleConv(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels: int, out_channels: int):
         super().__init__()
 
         self.seq = nn.Sequential(
@@ -33,12 +33,16 @@ class DoubleConv(nn.Module):
             DepthwiseConv(out_channels, out_channels),
         )
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor):
         return self.seq(x)
 
 
 class Down(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    """
+    Downscaling module in U-Net model.
+    """
+
+    def __init__(self, in_channels: int, out_channels: int):
         super().__init__()
 
         self.seq = nn.Sequential(
@@ -46,26 +50,39 @@ class Down(nn.Module):
             nn.MaxPool2d(kernel_size=2),
         )
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor):
         return self.seq(x)
 
 
 class Up(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    """
+    Upscaling module in U-Net model.
+
+    This upscales a feature map from the previous "up" stage of the network
+    and combines it with a feature map coming across from a "down" stage.
+    """
+
+    def __init__(self, in_up_channels: int, in_cross_channels: int, out_channels: int):
+        """
+        :param in_up_channels: Channels in inputs to be upscaled
+        :param in_cross_channels: Channels in inputs to be concatenated with upscaled input
+        """
         super().__init__()
 
-        self.up = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=3, stride=2)
-        self.contract = DoubleConv(in_channels, out_channels)
+        self.up = nn.ConvTranspose2d(
+            in_up_channels, out_channels, kernel_size=3, stride=2
+        )
+        self.contract = DoubleConv(out_channels + in_cross_channels, out_channels)
 
-    def forward(self, x_down, x):
-        x_up = self.up(x_down)
+    def forward(self, x_to_upscale: torch.Tensor, x: torch.Tensor):
+        upscaled = self.up(x_to_upscale)
+        pad_r = x.shape[3] - upscaled.shape[3]
+        pad_b = x.shape[2] - upscaled.shape[2]
+        upscaled = nn.functional.pad(upscaled, (0, pad_r, 0, pad_b))
 
-        pad_r = x.shape[3] - x_up.shape[3]
-        pad_b = x.shape[2] - x_up.shape[2]
-        x_up = nn.functional.pad(x_up, (0, pad_r, 0, pad_b))
-        x_up = torch.cat((x_up, x), dim=1)
+        combined = torch.cat((upscaled, x), dim=1)
 
-        return self.contract(x_up)
+        return self.contract(combined)
 
 
 class DetectionModel(nn.Module):
@@ -87,39 +104,35 @@ class DetectionModel(nn.Module):
         # reduced scale to cut down the parameter count.
         #
         # depth_scale = [64, 128, 256, 512, 1024]
-        depth_scale = [16, 32, 64, 128, 256]
+        depth_scale = [8, 16, 32, 32, 64, 128, 256]
+        self.depth_scale = depth_scale
 
         self.in_conv = DoubleConv(1, depth_scale[0])
 
-        self.down1 = Down(depth_scale[0], depth_scale[1])
-        self.down2 = Down(depth_scale[1], depth_scale[2])
-        self.down3 = Down(depth_scale[2], depth_scale[3])
-        self.down4 = Down(depth_scale[3], depth_scale[4])
+        self.down = nn.ModuleList()
+        for i in range(len(depth_scale) - 1):
+            self.down.append(Down(depth_scale[i], depth_scale[i + 1]))
 
-        self.up4 = Up(depth_scale[4], depth_scale[3])
-        self.up3 = Up(depth_scale[3], depth_scale[2])
-        self.up2 = Up(depth_scale[2], depth_scale[1])
-        self.up1 = Up(depth_scale[1], depth_scale[0])
+        self.up = nn.ModuleList()
+        for i in range(len(depth_scale) - 1):
+            self.up.append(Up(depth_scale[i + 1], depth_scale[i], depth_scale[i]))
 
-        # Number of output masks to generate
-        n_masks = 1
-
-        self.final = nn.Sequential(
+        n_masks = 1  # Output masks to generate
+        self.out_conv = nn.Sequential(
             nn.Conv2d(depth_scale[0], n_masks, kernel_size=1, padding="same"),
             nn.Sigmoid(),
         )
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor):
         x = self.in_conv(x)
 
-        x_down_1 = self.down1(x)
-        x_down_2 = self.down2(x_down_1)
-        x_down_3 = self.down3(x_down_2)
-        x_down_4 = self.down4(x_down_3)
+        x_down = []
+        for i, down_op in enumerate(self.down):
+            prev_down = x if i == 0 else x_down[-1]
+            x_down.append(down_op(prev_down))
 
-        x_up_4 = self.up4(x_down_4, x_down_3)
-        x_up_3 = self.up3(x_up_4, x_down_2)
-        x_up_2 = self.up2(x_up_3, x_down_1)
-        x_up_1 = self.up1(x_up_2, x)
+        x_up = x_down[-1]
+        for i, up_op in reversed(list(enumerate(self.up))):
+            x_up = up_op(x_up, x if i == 0 else x_down[i - 1])
 
-        return self.final(x_up_1)
+        return self.out_conv(x_up)
