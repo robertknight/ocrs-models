b16f1a9498b6115d291772c3133418284187a342
Robert Knight <robertknight@gmail.com>
2023-05-08 06:08:39 +0100

    Experiment with increased LSTM input size

--- text_detection/model.py
+++ text_detection/model.py
@@ -215,9 +215,9 @@ class RecognitionModel(nn.Module):
             nn.MaxPool2d(kernel_size=(2, 1)),
         )
         img_height = 64
-        self.linear = nn.Sequential(nn.Linear(img_height // 16 * 128, 128), nn.ReLU())
+        self.linear = nn.Sequential(nn.Linear(img_height // 16 * 128, 256), nn.ReLU())
 
-        self.lstm = nn.LSTM(128, 256, bidirectional=True, num_layers=2, dropout=0.2)
+        self.lstm = nn.LSTM(256, 256, bidirectional=True, num_layers=2, dropout=0.2)
 
         self.output = nn.Sequential(
             nn.Linear(512, n_classes),
