43e32e1ac5a538f5f8adff225589bed42b409cdb
Robert Knight <robertknight@gmail.com>
2023-09-05 07:48:17 +0100

    Add a tool to fetch a list of random Wikipedia URLs

---
+++ layout-scraper/tools/random-wiki-url-generator.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+/**
+ * Fetch `count` random Wikipedia URLs and log them to stdout.
+ */
+async function fetchURLs(count) {
+  for (let i = 0; i < count; i++) {
+    // nb. `Special:Random` returns a 302, `fetch` follows redirects
+    // automatically.
+    const response = await fetch(
+      "https://en.wikipedia.org/wiki/Special:Random",
+    );
+    if (!response.ok) {
+      console.error(`Non-OK response ${response.status}`);
+    }
+    console.error(`Fetched ${i+1} of ${count} URLs`);
+    console.log(response.url);
+  }
+}
+
+const count = parseInt(process.argv[2]);
+console.error(`Fetching ${count} random Wikipedia URLs...`);
+fetchURLs(count);
